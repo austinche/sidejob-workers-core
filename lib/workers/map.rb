3ea382d131d20d8aa908d0262e908efdedc47a25
Austin Che <austin@ginkgobioworks.com>
2015-04-22 22:15:44 +0000

    Map

---
+++ lib/workers/map.rb
@@ -0,0 +1,51 @@
+# Map is designed to work with a second job
+# An input array or object enters on input(:in) and each entry sent to output(:each)
+# From there, the elements are sent to the second job
+# The output value for each element should be fed back into input(:each)
+# Then Map outputs the mapped array to output(:out)
+module Workers
+  class Map
+    include SideJob::Worker
+    register(
+        description: 'Maps elements in an array or object',
+        icon: 'map-marker',
+        inports: {
+            in: { type: 'all', description: 'Input array or object' },
+            each: { type: 'all', description: 'Each mapped value' },
+        },
+        outports: {
+            each: { type: 'all', description: 'Elements of array or [key, value] from object' },
+            out: { type: 'array', description: 'Output array with mapped values' },
+        },
+    )
+
+    def perform
+      loop do
+        n = get(:n)
+        if ! n
+          return unless input(:in).data?
+          source = input(:in).read
+          raise 'Input must be an array or object' unless source.respond_to?(:each)
+          n = source.length
+          set({n: n})
+          source.each {|x| output(:each).write x}
+        end
+
+        values = get(:values) || []
+
+        while n > values.length && input(:each).data?
+          values << input(:each).read
+        end
+
+        if n == values.length
+          # got all mapped values, write out
+          output(:out).write values
+          unset('n', 'values')
+        else
+          set({values: values})
+          suspend
+        end
+      end
+    end
+  end
+end
---
+++ spec/workers/map_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe Workers::Map do
+  before do
+    @job = SideJob.queue('core', 'Workers::Map')
+  end
+
+  it 'completes on no input' do
+    @job.run_inline
+    expect(@job.status).to eq 'completed'
+  end
+
+  it 'takes input array and outputs elements' do
+    @job.input(:in).write((0..4).to_a)
+    @job.run_inline
+    expect(@job.status).to eq 'suspended'
+    expect(@job.output(:each).entries).to eq (0..4).to_a
+  end
+
+  it 'raises error if input is not an array' do
+    @job.input(:in).write(3)
+    expect { @job.run_inline }.to raise_error
+  end
+
+  it 'suspends on partial mapped values' do
+    @job.input(:in).write((0..4).to_a)
+    @job.input(:each).write('a')
+    @job.run_inline
+    expect(@job.status).to eq 'suspended'
+  end
+
+  it 'completes on all mapped values' do
+    @job.input(:in).write((1..3).to_a)
+    @job.input(:each).write('a')
+    @job.run_inline
+    @job.input(:each).write('b')
+    @job.input(:each).write('c')
+    @job.run_inline
+    expect(@job.output(:each).entries).to eq (1..3).to_a
+    expect(@job.output(:out).read).to eq ['a', 'b', 'c']
+    expect(@job.status).to eq 'completed'
+  end
+
+  it 'can map with a default value port' do
+    @job.input(:each).options = {mode: :memory}
+    @job.input(:in).write((1..3).to_a)
+    @job.input(:each).write('xyz')
+    @job.run_inline
+    expect(@job.status).to eq 'completed'
+    expect(@job.output(:out).read).to eq ['xyz', 'xyz', 'xyz']
+  end
+
+  it 'can map on an object' do
+    @job.input(:in).write({abc: 123, xyz: 456})
+    @job.run_inline
+    expect(@job.output(:each).entries).to match_array([['abc', 123], ['xyz', 456]])
+    @job.input(:each).write('a')
+    @job.input(:each).write('b')
+    @job.input(:each).write('c')
+    @job.run_inline
+    expect(@job.output(:out).read).to eq ['a', 'b']
+    expect(@job.status).to eq 'completed'
+  end
+
+end
