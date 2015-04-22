50a571b0559a6e4579b2363545e2be9ae7993f7f
Austin Che <austin@ginkgobioworks.com>
2015-04-22 14:37:35 +0000

    get rid of widget pubsub

--- lib/workers/widget.rb
+++ lib/workers/widget.rb
@@ -12,12 +12,7 @@ module Workers
     )
 
     def perform(widget_name=nil)
-      # Set status to a custom one and publish a message to potentially notify any listeners that
-      # this widget needs to be run for this job
-      self.status = 'user'
-      if ENV['PUBSUB_HOST']
-        Redis.new(host: ENV['PUBSUB_HOST']).publish("Widget:job/#{id}", {event: 'run', name: widget_name}.to_json)
-      end
+      suspend # suspends for the external widget to handle
     end
   end
 end
--- spec/workers/widget_spec.rb
+++ spec/workers/widget_spec.rb
@@ -5,24 +5,8 @@ describe Workers::Widget do
     @job = SideJob.queue('core', 'Workers::Widget')
   end
 
-  it 'sets status to user' do
+  it 'suspends' do
     @job.run_inline
-    expect(@job.status).to eq 'user'
-  end
-
-  it 'publishes a message via redis' do
-    ENV['PUBSUB_HOST'] = 'localhost'
-    redis = Redis.new(host: 'localhost')
-    Timeout.timeout(5) do
-      redis.subscribe("Widget:job/#{@job.id}") do |on|
-        on.subscribe do
-          @job.run_inline(args: ['MyWidget'])
-        end
-        on.message do |channel, message|
-          expect(JSON.parse(message)).to eq({'event' => 'run', 'name' => 'MyWidget'})
-          redis.unsubscribe
-        end
-      end
-    end
+    expect(@job.status).to eq 'suspended'
   end
 end
